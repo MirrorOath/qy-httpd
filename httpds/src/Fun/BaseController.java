3186da121ff9d192b0cb4721be8ba368e398962a
left <465717240@qq.com>
2018-05-23 14:19:11 +0800

    修改代码格式

--- httpds/src/Fun/BaseController.java
+++ httpds/src/Fun/BaseController.java
@@ -8,60 +8,59 @@ import com.jfinal.core.Controller;
 import com.jfinal.plugin.activerecord.Db;
 
 public class BaseController extends Controller {
-	public String _SQL = "";
+    public String _SQL = "";
 
-	public String Q(String s) {
-		return "'" + s + "'";
-	}	
+    public String Q(String s) {
+        return "'" + s + "'";
+    }
 
-	public  Date StrToDate(String sdate) {
-		if (isN(sdate)) {
-			return null;
-		} else {
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-			try {
-				Date t = sdf.parse(sdate);
-				return t;
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				return null;
-			}
-		}
-	}
+    public Date StrToDate(String sdate) {
+        if (isN(sdate)) {
+            return null;
+        } else {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            try {
+                Date t = sdf.parse(sdate);
+                return t;
+            } catch (ParseException e) {
+                // TODO Auto-generated catch block
+                return null;
+            }
+        }
+    }
 
-	public Date StrToDateTime(String sdate) {
-		if (isN(sdate)) {
-			return null;
-		} else {
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			try {
-				Date t = sdf.parse(sdate);
-				return t;
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				return null;
-			}
-		}
-	}
+    public Date StrToDateTime(String sdate) {
+        if (isN(sdate)) {
+            return null;
+        } else {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            try {
+                Date t = sdf.parse(sdate);
+                return t;
+            } catch (ParseException e) {
+                // TODO Auto-generated catch block
+                return null;
+            }
+        }
+    }
 
-	public String P(String p) {
-		String s = this.getPara(p);
-		if (s == null)
-			return "";
-		else
-			return s;
+    public String P(String p) {
+        String s = this.getPara(p);
+        if (s == null)
+            return "";
+        else
+            return s;
 
-	}
+    }
 
+    public Boolean isN(String value) {
+        if (value != null)
+            if (!value.trim().equals(""))
+                return false;
+            else
+                return true;
+        else
+            return true;
+    }
 
-	public Boolean isN(String value) {
-		if (value != null)
-			if (!value.trim().equals(""))
-				return false;
-			else
-				return true;
-		else
-			return true;
-	}
-	
 }
--- httpds/src/Fun/UtilsPuMonth.java
+++ httpds/src/Fun/UtilsPuMonth.java
@@ -7,79 +7,82 @@ import com.jfinal.plugin.activerecord.Record;
 
 /**
  * 公有方法类
+ * 
  * @author Administrator
  *
  */
-public class UtilsPuMonth  {
-	public static String Q(String s) {
-		return "'" + s + "'";
-	}	
-	/**
-	 * 保存及修改信息的公共方法
-	 * @param db
-	 * @param reginfo
-	 * @param czlist
-	 * @param qpczlist
-	 * @param zcdm
-	 * @param optype
-	 * @param times
-	 * @return
-	 */
-	public static boolean savedata1(String db, String reginfo, String czlist, String qpczlist, String zcdm, String optype,
-			String times) {
-		String sql = "select * from " + reginfo + " where (zcdm=" + Q(zcdm) + " or zcdmtsg08=" + Q(zcdm) + ")";
-		Record re = Db.use(db).findFirst(sql);
-		int typcode = 1;
-		try {
-			typcode = Integer.valueOf(optype);
+public class UtilsPuMonth {
+    public static String Q(String s) {
+        return "'" + s + "'";
+    }
 
-		} catch (Exception e) {
-			// TODO: handle exception
-			typcode = 1;
-		}
-		if (re != null) {
-			if ((re.getDate("LastXjrq_Jyjl").getTime() < new Date().getTime())
-					|| (re.getDate("LastBFRQ_Jyjl").getTime() < new Date().getTime())) {
-				sql = "insert into " + czlist
-						+ " (checkdatetime,rno,Syzbh,zcdm,Pcode,Type_name,Medium_name,Pid,Pdate,LastJydate,"
-						+ "LastXjrq_Jyjl,LastBFRQ_Jyjl,Safedate,InfoLabeltype,InfoLabel,Checkresult,updatetime,OpType) "
-						+ "select now(),RNO,Syzbh,zcdm,Pcode,Type_name,Medium_name,Pid,Pdate,LastJydate,LastXjrq_Jyjl,"
-						+ "LastBFRQ_Jyjl,BFRQ,InfoLabeltype,InfoLabeltype_name,'1',now()," + typcode + " from "
-						+ reginfo + " where zcdm=" + Q(re.getStr("zcdm"));
+    /**
+     * 保存及修改信息的公共方法
+     * 
+     * @param db
+     * @param reginfo
+     * @param czlist
+     * @param qpczlist
+     * @param zcdm
+     * @param optype
+     * @param times
+     * @return
+     */
+    public static boolean savedata1(String db, String reginfo, String czlist, String qpczlist, String zcdm,
+            String optype, String times) {
+        String sql = "select * from " + reginfo + " where (zcdm=" + Q(zcdm) + " or zcdmtsg08=" + Q(zcdm) + ")";
+        Record re = Db.use(db).findFirst(sql);
+        int typcode = 1;
+        try {
+            typcode = Integer.valueOf(optype);
 
-			} else {
-				sql = "insert into " + czlist
-						+ " (checkdatetime,rno,Syzbh,zcdm,Pcode,Type_name,Medium_name,Pid,Pdate,LastJydate,"
-						+ "LastXjrq_Jyjl,LastBFRQ_Jyjl,Safedate,InfoLabeltype,InfoLabel,Checkresult,updatetime,OpType)"
-						+ " select now(),RNO,Syzbh,zcdm,Pcode,Type_name,Medium_name,Pid,Pdate,LastJydate,LastXjrq_Jyjl,"
-						+ "LastBFRQ_Jyjl,BFRQ,InfoLabeltype,InfoLabeltype_name,'0',now()," + typcode + " from "
-						+ reginfo + " where zcdm=" + Q(re.getStr("zcdm"));
+        } catch (Exception e) {
+            // TODO: handle exception
+            typcode = 1;
+        }
+        if (re != null) {
+            if ((re.getDate("LastXjrq_Jyjl").getTime() < new Date().getTime())
+                    || (re.getDate("LastBFRQ_Jyjl").getTime() < new Date().getTime())) {
+                sql = "insert into " + czlist
+                        + " (checkdatetime,rno,Syzbh,zcdm,Pcode,Type_name,Medium_name,Pid,Pdate,LastJydate,"
+                        + "LastXjrq_Jyjl,LastBFRQ_Jyjl,Safedate,InfoLabeltype,InfoLabel,Checkresult,updatetime,OpType) "
+                        + "select now(),RNO,Syzbh,zcdm,Pcode,Type_name,Medium_name,Pid,Pdate,LastJydate,LastXjrq_Jyjl,"
+                        + "LastBFRQ_Jyjl,BFRQ,InfoLabeltype,InfoLabeltype_name,'1',now()," + typcode + " from "
+                        + reginfo + " where zcdm=" + Q(re.getStr("zcdm"));
 
-			}
-			String upsql = "select * from " + qpczlist + " where RNOID=" + Q(re.getStr("RNO"));
-			Record rs = Db.use(db).findFirst(upsql);
-			if (optype.equals("czjc")) {// 充装检查
-				upsql = "update " + reginfo + " set lczdw=" + Q(rs.get("Rname").toString()) + ",lczrq=" + Q(times) + ","
-						+ "lastczdz="+Q(rs.get("RAddress").toString()) + " where zcdm=" + Q(re.getStr("zcdm"));
-			}
-			if (optype.equals("czqjc") || optype.equals("1")) {// 充装前检查
-				upsql = "update " + reginfo + " set lczdw=" + Q(rs.get("Rname").toString()) + ",lczjcrq=" + Q(times)
-						+ ",lastczdz="+ Q(rs.get("RAddress").toString()) + " where zcdm=" + Q(re.getStr("zcdm"));
-			}
-			if (optype.equals("czfj") || optype.equals("2")) {// 充装后复检
-				upsql = "update " + reginfo + " set lczdw=" + Q(rs.get("Rname").toString()) + ",lczfjrq=" + Q(times)
-						+ ",lastczdz="+ Q(rs.get("RAddress").toString()) + " where zcdm=" + Q(re.getStr("zcdm"));
-			}
-			if (optype.equals("2")) {
+            } else {
+                sql = "insert into " + czlist
+                        + " (checkdatetime,rno,Syzbh,zcdm,Pcode,Type_name,Medium_name,Pid,Pdate,LastJydate,"
+                        + "LastXjrq_Jyjl,LastBFRQ_Jyjl,Safedate,InfoLabeltype,InfoLabel,Checkresult,updatetime,OpType)"
+                        + " select now(),RNO,Syzbh,zcdm,Pcode,Type_name,Medium_name,Pid,Pdate,LastJydate,LastXjrq_Jyjl,"
+                        + "LastBFRQ_Jyjl,BFRQ,InfoLabeltype,InfoLabeltype_name,'0',now()," + typcode + " from "
+                        + reginfo + " where zcdm=" + Q(re.getStr("zcdm"));
 
-			}
-			if (Db.use(db).update(sql) > 0 && Db.use(db).update(upsql) > 0) {
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			return false;
-		}
-	}
+            }
+            String upsql = "select * from " + qpczlist + " where RNOID=" + Q(re.getStr("RNO"));
+            Record rs = Db.use(db).findFirst(upsql);
+            if (optype.equals("czjc")) {// 充装检查
+                upsql = "update " + reginfo + " set lczdw=" + Q(rs.get("Rname").toString()) + ",lczrq=" + Q(times) + ","
+                        + "lastczdz=" + Q(rs.get("RAddress").toString()) + " where zcdm=" + Q(re.getStr("zcdm"));
+            }
+            if (optype.equals("czqjc") || optype.equals("1")) {// 充装前检查
+                upsql = "update " + reginfo + " set lczdw=" + Q(rs.get("Rname").toString()) + ",lczjcrq=" + Q(times)
+                        + ",lastczdz=" + Q(rs.get("RAddress").toString()) + " where zcdm=" + Q(re.getStr("zcdm"));
+            }
+            if (optype.equals("czfj") || optype.equals("2")) {// 充装后复检
+                upsql = "update " + reginfo + " set lczdw=" + Q(rs.get("Rname").toString()) + ",lczfjrq=" + Q(times)
+                        + ",lastczdz=" + Q(rs.get("RAddress").toString()) + " where zcdm=" + Q(re.getStr("zcdm"));
+            }
+            if (optype.equals("2")) {
+
+            }
+            if (Db.use(db).update(sql) > 0 && Db.use(db).update(upsql) > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
